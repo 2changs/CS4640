8938eac8a13a105e64d08b3d51fc1ddc331ede1a
2changs <diana.chang05@gmail.com>
2017-09-28 10:55:24 -0400

    i should commit more

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function validate() {
+    var a = document.forms["form-contact"]["name"].value;
+    var b = document.forms["form-contact"]["email"].value;
+    var c = document.forms["form-contact"]["phone"].value;
+    var d = document.forms["form-contact"]["message"].value;
+
+    if (a == null || a ==""){
+    	document.getElementById('error-name').innerHTML = '*Please enter a name.'
+    }
+    if (b == null || b ==""){
+    	document.getElementById('error-email').innerHTML = '*Please enter an email.'
+    }
+    if (c == null || c ==""){
+    	document.getElementById('error-phone').innerHTML = '*Please enter a phone number.'
+    }
+    if (d == null || d ==""){
+    	document.getElementById('error-message').innerHTML = '*Please enter a message.'
+    }
+
+}
+
+function goBack() {
+    window.history.back();
+}
